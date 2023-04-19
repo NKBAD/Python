dedd720b6c1e4b43278e3f338fe3437889307191
NKBAD <nicolas.silva.figueroa@alumnos.sip.cl>
2023-04-19 13:01:59 -0400

    python

--- python/fundamentals/extras/tienda/tienda.py
+++ python/fundamentals/extras/tienda/tienda.py
@@ -3,11 +3,26 @@ class tiendita:
         self.nombre = name
         self.producto = []
 
-
-    def add_producto(self,produc):
+    def add_producto(self, produc):
         self.producto.append(produc)
         return self
+
     def Print_info(self):
         for i in self.producto:
             print(f"{ i.nombre} {i.precio} {i.categoria} ")
         return self
+    
+    def vender(self, num, *nums):
+        self.result -= num
+        for n in nums:
+            self.result -= n
+
+    def inflacion(self, porcentaje , *nums):
+            self.result -= porcentaje    
+            for n in nums:
+                self.result += n
+
+    def hacer_liquidacion(self, num, *nums):
+            self.result -= num
+            for n in nums:
+                self.result -= n
